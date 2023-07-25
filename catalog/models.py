d55d4a5d734d232d37fc5b69c69f4c90405127bb
Violetta <lotsmanovavioletta@gmail.com>
2023-07-25 23:35:34 +0300

    fixed models and add migrate

--- catalog/models.py
+++ catalog/models.py
@@ -2,15 +2,32 @@ from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
 
+class Category(models.Model):
+    name = models.CharField(max_length=150,  verbose_name='наименование')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
 class Product(models.Model):
     name = models.CharField(max_length=150,  verbose_name='наименование')
     description = models.TextField(verbose_name='описание')
     image = models.ImageField(upload_to='product/', verbose_name='изображение', **NULLABLE)
-    category = models.ImageField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
     price = models.PositiveIntegerField(verbose_name='цена')
     date_create = models.DateField(verbose_name='дата создания', **NULLABLE)
     date_change = models.DateField(verbose_name='дата последнего изменения', **NULLABLE)
+    created_at = models.BooleanField(verbose_name='создан')
+
+    def __str__(self):
+        return f'{self.name} ({self.category})'
 
 
-class Category(models.Model):
-    pass
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
