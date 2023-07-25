4b0fb46a6a172fe64c6223bc4c35e1cb77e0333d
Violetta <lotsmanovavioletta@gmail.com>
2023-07-25 23:04:30 +0300

    add models product and settings for media

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+class Product(models.Model):
+    name = models.CharField(max_length=150,  verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+    image = models.ImageField(upload_to='product/', verbose_name='изображение', **NULLABLE)
+    category = models.ImageField()
+    price = models.PositiveIntegerField(verbose_name='цена')
+    date_create = models.DateField(verbose_name='дата создания', **NULLABLE)
+    date_change = models.DateField(verbose_name='дата последнего изменения', **NULLABLE)
+
+
+class Category(models.Model):
+    pass
