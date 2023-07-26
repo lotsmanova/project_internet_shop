ed67e594c23f5fd25e94b31172a4c4f3b3d9b7d9
Violetta <lotsmanovavioletta@gmail.com>
2023-07-26 23:14:58 +0300

    add model contact

--- catalog/models.py
+++ catalog/models.py
@@ -24,9 +24,27 @@ class Product(models.Model):
     date_create = models.DateField(verbose_name='дата создания', **NULLABLE)
     date_change = models.DateField(verbose_name='дата последнего изменения', **NULLABLE)
 
+
     def __str__(self):
         return f'{self.name} ({self.category})'
+
 
     class Meta:
         verbose_name = 'товар'
         verbose_name_plural = 'товары'
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=150, verbose_name='компания')
+    email = models.EmailField(verbose_name='email')
+    phone = models.CharField(max_length=50, verbose_name='телефон')
+    address = models.CharField(max_length=150, verbose_name='адрес')
+
+
+    def __str__(self):
+        return f'{self.name} ({self.address})'
+
+
+    class Meta:
+        verbose_name = 'контакты'
+        verbose_name_plural = 'контакты'
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
+from catalog.models import Product, Contact
+
+
 def home_page(request):
-    return render(request, 'catalog/home_page.html')
+    latest_products = Product.objects.order_by('-date_create')[:5]
+    for product in latest_products:
+        print(product.name, product.price)
+        context = {
+            'latest_products': latest_products,
+        }
+    return render(request, 'catalog/home_page.html', context)
+
 
 def contact_page(request):
-    return render(request, 'catalog/contact_page.html')
+    contacts = Contact.objects.all()
+    context = {
+        'contacts': contacts,
+    }
+    return render(request, 'catalog/contact_page.html', context)
