1fd1c758fb14adf25deaf1da200b5ea6ad817b45
Violetta <lotsmanovavioletta@gmail.com>
2023-07-29 14:07:54 +0300

    fixed contact_page

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,6 @@ from catalog.views import home_page, contact_page
 
 urlpatterns = [
     path('', home_page, name='home_page'),
-    path('templates/', contact_page, name='contact_page'),
+    path('contacts/', contact_page, name='contact_page'),
 ]
 
