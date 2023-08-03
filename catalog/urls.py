2aba42924f2bddbfa789de0579ac65893efd7430
Violetta <lotsmanovavioletta@gmail.com>
2023-08-03 23:05:54 +0300

    add model Blog

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import HomeListView, ContactListView, ProductListView, ProductCreateView, CategoryListView
+from catalog.views import HomeListView, ContactListView, ProductListView, ProductCreateView, CategoryListView, \
+    BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView
 
 app_name = CatalogConfig.name
 
@@ -10,6 +11,12 @@ urlpatterns = [
     path('contacts/', ContactListView.as_view(), name='contact_page'),
     path('categories/', CategoryListView.as_view(), name='categories'),
     path('<int:pk>/products/', ProductListView.as_view(), name='product_page'),
-    path('add_product/', ProductCreateView.as_view(), name='add_product')
+    path('add_product/', ProductCreateView.as_view(), name='add_product'),
+    path('blog_create/', BlogCreateView.as_view(), name='blog_create'),
+    path('blog_detail/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog_update/<int:pk>/', BlogUpdateView.as_view(), name='blog_update'),
+    path('blog_delete/<int:pk>/', BlogDeleteView.as_view(), name='blog_delete'),
+    path('blog_list/', BlogListView.as_view(), name='blog_list'),
+
 ]
 
